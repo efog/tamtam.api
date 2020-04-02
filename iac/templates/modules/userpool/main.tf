f292a9c65dbcd687b8e7e4bd32f51ba955091074
Etienne Brouillard <etienne.brouillard@outlook.com>
2020-04-02 18:00:59 -0400

    email invite template

--- iac/templates/modules/userpool/main.tf
+++ iac/templates/modules/userpool/main.tf
@@ -13,9 +13,16 @@ resource "aws_route53_record" "users_domain_a_record" {
 resource "aws_cognito_user_pool" "pool" {
   name = "tamtam_userpool_${var.tags.env}"
   tags = var.tags
+  admin_create_user_config {
+    invite_message_template {
+      email_message = file("${path.module}/templates/emailinvitetemplate.html")
+      email_subject = "Your invitation to register at Tamtam"
+      sms_message   = file("${path.module}/templates/smsinvitetemplate.txt")
+    }
+  }
   email_configuration {
     email_sending_account = "DEVELOPER"
-    source_arn            = "arn:aws:ses:${var.region}:${var.aws_account_number}:identity/userpool.${var.tags.env}@efog.ca"
+    source_arn            = "arn:aws:ses:${var.region}:${var.aws_account_number}:identity/userpool.${var.tags.env}@${var.domain}"
   }
 }
 
