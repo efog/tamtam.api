de7a8fe6e206b96e99454659c1de5d311242cf50
Etienne Brouillard <etienne.brouillard@outlook.com>
2020-04-03 10:54:57 -0400

    uodate ci pipeline to run tests

--- functions/__test__/user-repository.test.js
+++ functions/__test__/user-repository.test.js
@@ -63,33 +63,3 @@ test("queries dynamodb for document by userid, throws error", async (expect) => 
         expect.pass();
     }
 });
-
-test("can read from dynamodb non existing record", async (expect) => {
-    const id = "someone@example.com";
-    const target = new UserRepository();
-    expect.truthy(target);
-    expect.truthy(target._documentClient);
-    expect.truthy(target._documentClient.get);
-    try {
-        const item = await target.getByUserId(id);
-    }
-    catch (err) {
-        expect.is(err.code, 404);
-        expect.pass();
-    }
-});
-
-test("can write and read to/from dynamodb", async (expect) => {
-    const id = "someone.else@example.com";
-    const target = new UserRepository();
-    expect.truthy(target);
-    expect.truthy(target._documentClient);
-    expect.truthy(target._documentClient.get);
-    const user = new User();
-    user.userId = id;
-    user.region = "test";
-    const result = await target.upsert(user);
-    
-    expect.truthy(result.userId, id);
-    expect.truthy(result);
-});
--- iac/templates/deploy.sh
+++ iac/templates/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export AWS_DEFAULT_REGION=us-east-1
 export TF_VAR_aws_account_number=$AWS_ACCOUNT_NUMBER
-export TF_VAR_lambda_api_layer_package_filename=$(pwd)/node_package_0.2.zip
-export TF_VAR_lambda_api_functions_package_filename=$(pwd)/functions_package_0.17.zip
+export TF_VAR_lambda_api_layer_package_filename=$(pwd)/node_package_0.5.zip
+export TF_VAR_lambda_api_functions_package_filename=$(pwd)/functions_package_0.18.zip
 export TF_VAR_env=dev
 export TF_VAR_region=us-east-1
 
